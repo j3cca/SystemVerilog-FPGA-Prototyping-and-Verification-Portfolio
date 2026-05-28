4515cfa042ed8233c5f3717c55b629fb7dbae536
Jessica Milner <80304966+j3cca@users.noreply.github.com>
2026-05-28 15:58:56 -0400

    added rtl files

---
+++ 01-Combinational-Logic/4-Bit-Greater-Than-Comparator/src/greater_2bit_top.sv
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/23/2026 02:10:39 AM
+// Design Name: 
+// Module Name: bit2_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module greater_2bit_top(
+    input logic A1, //MSB1
+    input logic A0, //LSB1
+    input logic B1, //MSB2
+    input logic B0, //LSB2
+    output logic externalLED,
+    output logic onboardLED
+    );
+    
+    assign externalLED = (A1 & !B1) | (A0 & !B1 & !B0) | (A1 & A0 & !B0);
+    assign onboardLED = ~externalLED;
+    
+    
+endmodule
---
+++ 01-Combinational-Logic/4-Bit-Greater-Than-Comparator/src/greater_4bit_top.sv
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/27/2026 05:46:25 PM
+// Design Name: 
+// Module Name: greater_4bit_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module greater_4bit_top(
+    input logic A3_4, //A_MSB
+    input logic A2_4,
+    input logic A1_4,
+    input logic A0_4, //A_LSB
+    input logic B3_4, //B_MSB
+    input logic B2_4,
+    input logic B1_4,
+    input logic B0_4, //B_LSB
+    output logic externalLED,
+    output logic onboardLED
+    );
+    
+    logic gr_MSB_result;
+    logic eq_MSB_result;
+    logic gr_LSB_result;
+
+    greater_2bit_top gr_MSB_comp( 
+        .A1(A3_4),
+        .A0(A2_4),
+        .B1(B3_4),
+        .B0(B2_4),
+        .externalLED(gr_MSB_result),
+        .onboardLED()
+    );
+    
+    equal_2bit_top eq_MSB_comp( 
+        .A1(A3_4),
+        .A0(A2_4),
+        .B1(B3_4),
+        .B0(B2_4),
+        .externalLED(eq_MSB_result),
+        .onboardLED()
+    );
+    
+        greater_2bit_top gr_LSB_comp( 
+        .A1(A1_4),
+        .A0(A0_4),
+        .B1(B1_4),
+        .B0(B0_4),
+        .externalLED(gr_LSB_result),
+        .onboardLED()
+    );
+    
+    assign externalLED = (gr_MSB_result) | (eq_MSB_result & gr_LSB_result);
+    assign onboardLED = ~externalLED;
+    
+endmodule
